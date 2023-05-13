4233c4cdceac43ec740637c4ddb25a247c5214f9
Erundil95 <cristianferrari95@gmail.com>
2023-05-13 22:03:40 +0200

    added template json on missing file

---
+++ README.md
@@ -0,0 +1,5 @@
+
+MangaDelivery
+MangaDelivery is a Python-based application that allows users to automatically download and save manga chapters from specific websites. The application is designed to run on a server and store the downloaded manga chapters in a local directory or a cloud storage system of the user's choice.
+
+Project Structure
--- bin/main.py
+++ bin/main.py
@@ -1,26 +1,35 @@
-import requests
-import re
 import os
-import io
-import zipfile
-import sys
 import json
-from bs4 import BeautifulSoup
-from io import BytesIO
-from PIL import Image
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch
-from reportlab.lib.utils import ImageReader
-
+import sys
 from manga_scraper import MangaScraper
 from config_loader import ConfigLoader
 
+def create_default_config(config_file_path):
+    default_config = {
+        "save_folder": "MangaDeliveryTest",
+        "base_url": "https://onepiecechapters.com",
+        "mangalist_url": "https://onepiecechapters.com/projects",
+        "titles_to_download": [],
+        "save_format": "cbz"
+    }
+
+    print("Generating default one, please fill in the manga list")
+    with open(config_file_path, 'w') as f:
+        json.dump(default_config, f, indent=2)
+    
+
 if __name__ == "__main__":
     # Load JSON config file = current_dir path + going up one level with '..' + config + config.json
-    config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'config', 'config.json')
-    config_loader = ConfigLoader(config_file_path)
-    config = config_loader.load_config()
+    config_file_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'config.json'))
+
+    try:
+        config_loader = ConfigLoader(config_file_path)
+        config = config_loader.load_config()
+    except FileNotFoundError:
+        print(f"@@ Config file not found at {config_file_path}")
+        create_default_config(config_file_path)
+        sys.exit()
+
 
     # Config = Dictionary <key_setting> : string/list <value>d
     manga_scraper = MangaScraper(config)
